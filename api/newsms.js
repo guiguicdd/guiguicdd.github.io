96a9bfeb64530c81a404e21c164440c959c2f906
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2021-08-25 17:38:24 -0300

    Update newsms.js

--- api/newsms.js
+++ api/newsms.js
@@ -1,31 +1,37 @@
+const firebase = require('firebase-admin');
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require('firebase/database');
+require("firebase/firestore");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD0FRqr2Ifc7gNxoo07SRDG7bAasYPxeEU",
+    authDomain: "jarvisnodejs.firebaseapp.com",
+    databaseURL: "https://jarvisnodejs-default-rtdb.firebaseio.com",
+    projectId: "jarvisnodejs",
+    storageBucket: "jarvisnodejs.appspot.com",
+    messagingSenderId: "247934763889",
+    appId: "1:247934763889:web:98dd74547a32e0bff52cd0",
+    measurementId: "G-FTW747PLZ5"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
 module.exports = (req, res) => {
 
-    const firebase = require('firebase-admin');
-    // Add the Firebase products that you want to use
-    require("firebase/auth");
-    require('firebase/database');
-    require("firebase/firestore");
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyD0FRqr2Ifc7gNxoo07SRDG7bAasYPxeEU",
-        authDomain: "jarvisnodejs.firebaseapp.com",
-        databaseURL: "https://jarvisnodejs-default-rtdb.firebaseio.com",
-        projectId: "jarvisnodejs",
-        storageBucket: "jarvisnodejs.appspot.com",
-        messagingSenderId: "247934763889",
-        appId: "1:247934763889:web:98dd74547a32e0bff52cd0",
-        measurementId: "G-FTW747PLZ5"
-    };
-
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-
-    firebase.database().ref('users/' + '1').set({
-        username: 'as',
-        email: 'as@s.g',
-        profile_picture: 'sssssssssssssssss'
+    const usersDb = db.collection('users');
+    const liam = usersDb.doc('lragozzine');
+    liam.set({
+        first: 'Liam',
+        last: 'Ragozzine',
+        address: '133 5th St., San Francisco, CA',
+        birthday: '05/13/1990',
+        age: '30'
     });
-
 
 
 
