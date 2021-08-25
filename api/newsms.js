8654c426c2489e330967b36ad90f7220aa68c3da
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2021-08-25 15:06:38 -0300

    Update newsms.js

--- api/newsms.js
+++ api/newsms.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
 
-    var firebase = require("firebase/app");
+    var firebase = require("firebase");
     // Add the Firebase products that you want to use
     require("firebase/auth");
     require('firebase/database');
