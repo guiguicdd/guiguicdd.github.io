1d2e8843bf0520fb1a1e697eb7d48d74ddc4fcab
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2021-08-25 10:07:28 -0300

    Update newsms.js

--- api/newsms.js
+++ api/newsms.js
@@ -13,76 +13,22 @@ module.exports = (req, res) => {
     var numero = req.query.numero
 
     const uri = "mongodb+srv://newuser:6cUpDxKBWqbP1NKw@cluster0.0jncy.mongodb.net/jarvismessages?retryWrites=true&w=majority";
-    // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    // client.connect(err => {
-    //     const collection = client.db("smsz").collection("messages");
-    //     var myobj {
-    //         numer
-    //     }
-    //     collection.insertOne(myobj, function (err, res) {
-    //         if (err) throw err;
-    //         console.log("1 document inserted");
-    //         db.close();
-    //     });
-
-    //     // perform actions on the collection object
-
-
-    //     res.json({
-    //         status: "Pendente",
-    //         to: numero,
-    //         position: "",
-    //         mensagem: "Aguarde. Em breve o bot estará enviando a mensagem.",
-    //         otherinfo: {
-    //             bd: "findResult",
-    //             mensagem: mensagem,
-    //             numero: numero,
-    //             query: req.query,
-    //         }
-    //     });
-    //     client.close();
-    // });
-
-
-    MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("smsz");
-        var myobj = { numero: numero, mensagem: mensagem };
-        dbo.collection("messages").insertOne(myobj, function (err, resp) {
-            if (err) throw err;
-            console.log("1 document inserted");
-
-
-
-            res.json({
-                status: "Pendente",
-                to: numero,
-                position: "",
-                mensagem: "Aguarde. Em breve o bot estará enviando a mensagem.",
-                otherinfo: {
-                    bd: "findResult",
-                    mensagem: mensagem,
-                    numero: numero,
-                    query: req.query,
-                }
-            });
-
-
-
-            //   db.close();
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("smsz").collection("messages");
+        // perform actions on the collection object
+        res.json({
+            status: "Pendente",
+            to: numero,
+            position: "",
+            mensagem: "Aguarde. Em breve o bot estará enviando a mensagem.",
+            otherinfo: {
+                bd: "findResult",
+                mensagem: mensagem,
+                numero: numero,
+                query: req.query,
+            }
         });
+        client.close();
     });
-
-
-
-
-    // sms_data = {
-    //     mensagem: mensagem,
-    //     numero: numero
-    // };
-    // bd.push(sms_data);
-    // fs.writeFile(__dirname + '/sms.json', JSON.stringify(bd), (err) => {
-    //     if (err) throw err;
-    //     console.log('Build time file created successfully!');
-    //   })
 };
