173e6cef6aa6032d98deffdf56d2ff19cad4e93d
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2021-08-25 09:53:55 -0300

    Update newsms.js

--- api/newsms.js
+++ api/newsms.js
@@ -13,32 +13,66 @@ module.exports = (req, res) => {
     var numero = req.query.numero
 
     const uri = "mongodb+srv://newuser:6cUpDxKBWqbP1NKw@cluster0.0jncy.mongodb.net/jarvismessages?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const collection = client.db("smsz").collection("messages");
-        var myobj = {
-            numero: numero,
-            mensagem: mensagem
-        }
-        collection.insertOne(myobj, function (err, res) {
+    // const client = new MongoClient(uri);
+    // client.connect(err => {
+    //     const collection = client.db("smsz").collection("messages");
+    //     var myobj = {
+    //         numero: numero,
+    //         mensagem: mensagem
+    //     }
+    //     collection.insertOne(myobj, function (err, res) {
+    //         if (err) throw err;
+    //         console.log("1 document inserted");
+    //     });
+
+    //     // perform actions on the collection object
+
+
+    //     res.json({
+    //         status: "Pendente",
+    //         to: numero,
+    //         position: "",
+    //         mensagem: "Aguarde. Em breve o bot estará enviando a mensagem.",
+    //         otherinfo: {
+    //             bd: "findResult",
+    //             mensagem: mensagem,
+    //             numero: numero,
+    //             query: req.query,
+    //         }
+    //     });
+    // });
+
+
+
+
+
+
+    MongoClient.connect(uri, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        var myobj = { name: "Company Inc", address: "Highway 37" };
+        dbo.collection("customers").insertOne(myobj, function (err, res) {
             if (err) throw err;
             console.log("1 document inserted");
-        });
-
-        // perform actions on the collection object
 
 
-        res.json({
-            status: "Pendente",
-            to: numero,
-            position: "",
-            mensagem: "Aguarde. Em breve o bot estará enviando a mensagem.",
-            otherinfo: {
-                bd: "findResult",
-                mensagem: mensagem,
-                numero: numero,
-                query: req.query,
-            }
+
+            res.json({
+                status: "Pendente",
+                to: numero,
+                position: "",
+                mensagem: "Aguarde. Em breve o bot estará enviando a mensagem.",
+                otherinfo: {
+                    bd: "findResult",
+                    mensagem: mensagem,
+                    numero: numero,
+                    query: req.query,
+                }
+            });
+
+
+
+            //   db.close();
         });
     });
 
