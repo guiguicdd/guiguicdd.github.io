7b8d420c261b6cdaaa8b360222f8a219b783ac03
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2021-08-25 02:30:07 -0300

    Jarvis -Website - 1.0.010

--- api/newsms.js
+++ api/newsms.js
@@ -1,12 +1,17 @@
 module.exports = (req, res) => {
 
-    console.log(req.body);
-    console.log(req.query);
-    console.log(req.cookies);
+    console.log();
+    console.log();
+    console.log();
 
     res.json({
         status: "Pendente",
         to: "00000000000",
         position: "",
+        otherinfo: {
+            body: req.body,
+            query: req.query,
+            cookies: req.cookies
+        }
     });
 };
