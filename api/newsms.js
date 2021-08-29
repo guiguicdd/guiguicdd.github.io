7f34dfb6f003135db506b9722dfda54ab2284189
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2021-08-28 23:51:33 -0300

    Jarvis -Website - 1.2.009

--- api/newsms.js
+++ api/newsms.js
@@ -4,10 +4,15 @@ import { createClient } from '@supabase/supabase-js'
 
 
 module.exports = async (req, res) => {
-    const supabase = createClient("https://vhduhnycrkeomzsudlyl.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDIwMjMyOCwiZXhwIjoxOTQ1Nzc4MzI4fQ.Qh1JWMTOUgpZxUtR5aPhOhD0Om-euVoiTTlvm4bJ870")
+    if (!req.query.numero || !req.query.mensagem) return res.json({
+        status: "Failed",
+        mensagem: "Algum erro de identação"
+    });
 
     const mensagem = req.query.mensagem
     const numero = req.query.numero
+
+    const supabase = createClient("https://vhduhnycrkeomzsudlyl.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDIwMjMyOCwiZXhwIjoxOTQ1Nzc4MzI4fQ.Qh1JWMTOUgpZxUtR5aPhOhD0Om-euVoiTTlvm4bJ870")
 
     var { data, error } = await supabase
         .from('test')
@@ -16,36 +21,28 @@ module.exports = async (req, res) => {
     if (JSON.stringify(data).includes(numero)) return res.json({
         status: "InLine",
         to: numero,
-        position: "",
         mensagem: "O número que você está tendando enviar uma mensagem, já está no banco de dados."
     });
-
-
 
     console.log(JSON.stringify(data));
     console.log(JSON.stringify(error));
 
-    // var { data, error } = await supabase
-    //     .from('test')
-    //     .insert([
-    //         { message: mensagem, numero: numero }
-    //     ])
+    var { data, error } = await supabase
+        .from('test')
+        .insert([
+            { message: mensagem, numero: numero }
+        ])
 
-    // console.log('Massa' + JSON.stringify(data));
-    // console.log('Vix' + JSON.stringify(error));
-
-
+    console.log('Massa' + JSON.stringify(data));
+    console.log('Vix' + JSON.stringify(error));
 
 
 
 
 
-    if (!req.query.numero || !req.query.mensagem) return res.json({
-        status: "Failed",
-        to: "00000000000",
-        position: "",
-        mensagem: "Algum erro de identação"
-    });
+
+
+
 
     // 6cUpDxKBWqbP1NKw
 
@@ -53,10 +50,8 @@ module.exports = async (req, res) => {
     res.json({
         status: "Pendente",
         to: numero,
-        position: "",
         mensagem: "Aguarde. Em breve o bot estará enviando a mensagem.",
-        otherinfo: {
-            bd: "findResult",
+        checkinfo: {
             mensagem: mensagem,
             numero: numero,
             query: req.query,
