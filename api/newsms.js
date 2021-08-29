655d5df07220f5c6f12c198e4e596f243471b825
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2021-08-28 23:11:38 -0300

    Jarvis -Website - 1.2.000

--- api/newsms.js
+++ api/newsms.js
@@ -1,37 +1,16 @@
-const firebase = require('firebase-admin');
-// Add the Firebase products that you want to use
-require("firebase/auth");
-require('firebase/database');
-require("firebase/firestore");
+import { createClient } from '@supabase/supabase-js'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD0FRqr2Ifc7gNxoo07SRDG7bAasYPxeEU",
-    authDomain: "jarvisnodejs.firebaseapp.com",
-    databaseURL: "https://jarvisnodejs-default-rtdb.firebaseio.com",
-    projectId: "jarvisnodejs",
-    storageBucket: "jarvisnodejs.appspot.com",
-    messagingSenderId: "247934763889",
-    appId: "1:247934763889:web:98dd74547a32e0bff52cd0",
-    measurementId: "G-FTW747PLZ5"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
-
-module.exports = (req, res) => {
+// Create a single supabase client for interacting with your database 
+const supabase = createClient("https://vhduhnycrkeomzsudlyl.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDIwMjMyOCwiZXhwIjoxOTQ1Nzc4MzI4fQ.Qh1JWMTOUgpZxUtR5aPhOhD0Om-euVoiTTlvm4bJ870")
 
 
-    const usersDb = db.collection('users');
-    const liam = usersDb.doc('lragozzine');
-    liam.set({
-        first: 'Liam',
-        last: 'Ragozzine',
-        address: '133 5th St., San Francisco, CA',
-        birthday: '05/13/1990',
-        age: '30'
-    });
+module.exports = async (req, res) => {
+
+    const { data, error } = await supabase
+        .from('cities')
+        .insert([
+            { name: 'The Shire', country_id: 554 }
+        ])
 
 
 
