a0a588f9f600d3acd61fdd132c1acf13715c4073
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2021-08-29 03:37:57 -0300

    Jarvis -Website - 1.2.0016

--- api/newsms.js
+++ api/newsms.js
@@ -4,16 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-    }
+
     if (!req.query.numero || !req.query.mensagem) return res.json({
         status: "Failed",
         mensagem: "Algum erro de identação"
